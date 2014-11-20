a8b89571436bb354b2db8a6415f05ea8723c83b8
andrea beccaris <andreabeccaris@andreas-MacBook-Pro-5.local>
2014-11-20 18:04:28 +0100

    gauge done. Ready for bar

--- src/it/loadingthesis/gauge/Gauge.java
+++ src/it/loadingthesis/gauge/Gauge.java
@@ -35,6 +35,7 @@ public class Gauge extends View {
 	public Gauge(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		Log.d("constr","3");
+		path = new Path();
 		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		paint.setColor(0xff33ccff);//Color for background
 		paint.setStyle(Style.FILL);
@@ -43,12 +44,12 @@ public class Gauge extends View {
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-		
-//		setMinimumHeight(300);
-//		setMinimumWidth(300);
-//		setMeasuredDimension(500, 500);
+
+		//		setMinimumHeight(300);
+		//		setMinimumWidth(300);
+		//		setMeasuredDimension(500, 500);
 	}
-	
+
 	@Override
 	protected void onDraw(Canvas canvas) {
 		int w = this.getWidth();
@@ -62,6 +63,39 @@ public class Gauge extends View {
 		paint.setShader(new LinearGradient(0, r, 0, -r, Color.BLACK, Color.GRAY, TileMode.CLAMP));
 		canvas.drawCircle(0, 0, r*0.93f, paint);
 		
-	}
+		paint.setShader(new LinearGradient(-r,r,r,0,				
+				new int[]{Color.GREEN,Color.YELLOW,Color.RED}, null, TileMode.CLAMP));
+		
+		Log.d("x 0",(float) Math.cos(4/3*Math.PI)*r+"");
+		Log.d("y 1",(float) Math.sin(2/4*Math.PI)*(-r)+"");
+		Log.d("x 1",(float) Math.cos(2/4*Math.PI)*(r)+"");
+		Log.d("y 0",(float) Math.sin(4/3*Math.PI)*(-r)+"");
+		r = r*0.85f;
+		if(path.isEmpty()){
+			for (int i = 0; i <= 124; i++) {
+				float a = (float)(Math.PI*i/100);
+				float x = (float)(Math.cos(a)*r);
+				float y = (float)(Math.sin(a)*(-r));
+				if(i==0) path.moveTo(x, y);
+				else path.lineTo(x, y);
+			}
+			
+			r = r*0.90f;
+			for (int i = 124; i >= 0; i--) {
+				float a = (float)(Math.PI*i/100);
+				float x = (float)(Math.cos(a)*r);
+				float y = (float)(Math.sin(a)*(-r));
+				if(i==123) path.lineTo(x, y);
+				else path.lineTo(x, y);
+			}
+			path.close();
+		}
 
+
+		canvas.drawPath(path, paint);
+
+
+
+
+	}
 }
