dae067ddfad0be52694ffee83f0f89892a6bcad7
andrea beccaris <andreabeccaris@andreas-MacBook-Pro-5.local>
2014-11-20 14:19:36 +0100

    image view

--- src/it/loadingthesis/gauge/Gauge.java
+++ src/it/loadingthesis/gauge/Gauge.java
@@ -1,14 +1,19 @@
 package it.loadingthesis.gauge;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Paint.Style;
 import android.graphics.Path;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Gauge extends View {
 
 	private Paint paint;
+	private ImageView imageView;
 	private Path path;
 
 	public Gauge(Context context) {
@@ -23,7 +28,36 @@ public class Gauge extends View {
 
 	public Gauge(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
-		// TODO Auto-generated constructor stub
+
+		imageView = (ImageView)findViewById(R.id.imageView);
+		imageView.setDrawingCacheEnabled(true);
+		paint = new Paint();
+		paint.setColor(0x33ccff);//Color for background
+		paint.setStyle(Style.FILL);
+
+		path = new Path();
+
+		setMinimumWidth(100);
+		setMinimumHeight(100);
+
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		if(path.isEmpty()){
+			int w = canvas.getWidth();
+			int h = canvas.getHeight();
+			for (int i = 0; i < 100; i++) {
+				float a = (float)(Math.PI*2*i/100);
+				float x = (float)(Math.cos(a)*w/3+w/2);
+				float y = (float)(Math.sin(2*a)*h/3+h/2);
+				if(i==0) path.moveTo(x, y);
+				else path.lineTo(x, y);
+			}
+			path.close();
+		}
+		canvas.drawPath(path, paint);
+		imageView.draw(canvas);
 	}
 
 }
--- src/it/loadingthesis/gauge/MainActivity.java
+++ src/it/loadingthesis/gauge/MainActivity.java
@@ -4,9 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 
 public class MainActivity extends Activity {
+	
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
