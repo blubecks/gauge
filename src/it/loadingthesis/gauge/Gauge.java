8cdcc8e5859de0ac9f799dbf09ab8b818e4f887b
andrea beccaris <andreabeccaris@andreas-MacBook-Pro-5.local>
2014-11-20 15:18:39 +0100

    malnati eroe

--- src/it/loadingthesis/gauge/Gauge.java
+++ src/it/loadingthesis/gauge/Gauge.java
@@ -2,8 +2,11 @@ package it.loadingthesis.gauge;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.graphics.Shader.TileMode;
 import android.graphics.Path;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -32,7 +35,7 @@ public class Gauge extends View {
 	public Gauge(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 		Log.d("constr","3");
-		paint = new Paint();
+		paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		paint.setColor(0xff33ccff);//Color for background
 		paint.setStyle(Style.FILL);
 	}
@@ -41,18 +44,24 @@ public class Gauge extends View {
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 		
-		setMinimumHeight(300);
-		setMinimumWidth(300);
-		setMeasuredDimension(500, 500);
+//		setMinimumHeight(300);
+//		setMinimumWidth(300);
+//		setMeasuredDimension(500, 500);
 	}
 	
 	@Override
 	protected void onDraw(Canvas canvas) {
-		Integer w = canvas.getWidth();
-		Integer h = canvas.getHeight();
-		Log.d("heigth",h.toString());
-		Log.d("width",w.toString());
-		canvas.drawRect(0, 0, w, h, paint);
+		int w = this.getWidth();
+		int h = this.getHeight();
+		canvas.translate(w/2,h/2);
+		float r = Math.min(w, h)*0.48f;
+		paint.setShader(new LinearGradient(0, r, 0, -r, Color.GRAY, Color.LTGRAY, TileMode.CLAMP));
+		canvas.drawCircle(0, 0, r, paint);
+		paint.setShader(new LinearGradient(0, r, 0, -r, Color.LTGRAY, Color.GRAY, TileMode.CLAMP));
+		canvas.drawCircle(0, 0, r*0.95f, paint);
+		paint.setShader(new LinearGradient(0, r, 0, -r, Color.BLACK, Color.GRAY, TileMode.CLAMP));
+		canvas.drawCircle(0, 0, r*0.93f, paint);
+		
 	}
 
 }
--- src/it/loadingthesis/gauge/MainActivity.java
+++ src/it/loadingthesis/gauge/MainActivity.java
@@ -14,8 +14,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(new Gauge(this));
+        setContentView(R.layout.activity_main);
+        //setContentView(new Gauge(this));
         /*imageView = (ImageView)findViewById(R.id.imageView);
 		imageView.setDrawingCacheEnabled(true);*/
     }
