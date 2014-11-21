90b274098444bd7945b69f419e0341127598f8b8
blubecks <andreabeccaris88@gmail.com>
2014-11-21 19:44:46 +0100

    Create Buttons to set angle

--- src/it/loadingthesis/gauge/MainActivity.java
+++ src/it/loadingthesis/gauge/MainActivity.java
@@ -2,21 +2,30 @@ package it.loadingthesis.gauge;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
 import android.widget.SeekBar;
 
 
 public class MainActivity extends Activity {
 
 	//private ImageView imageView;
+	private Gauge g;
+	private float delta = 0;
+	private SeekBar seek;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		final Gauge g = (Gauge) findViewById(R.id.gauge);
-		SeekBar seek = (SeekBar) findViewById(R.id.seek);
+		g = (Gauge) findViewById(R.id.gauge);
+		seek = (SeekBar) findViewById(R.id.seek);
 
 		seek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
@@ -37,8 +46,38 @@ public class MainActivity extends Activity {
 
 			}
 		});
+
+
+		final Button inc = (Button)findViewById(R.id.increase);
+		final Button dec = (Button)findViewById(R.id.decrese);
+		inc.setOnTouchListener(new OnTouchListener() {
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+				if(event.getAction()==MotionEvent.ACTION_UP)
+					Log.i("", "Long press!");
+
+				pressDegree();
+				return false;
+			}
+		});
+
+
+
 	}
 
+	private void pressDegree() {
+		if(delta !=225){
+			if (delta!=220)
+				delta+=10;
+			else delta+=5;
+		}
+		seek.setProgress((int) (delta/2.25));
+		g.setDegres(delta);
+
+
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
